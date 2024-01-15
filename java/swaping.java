7fe548ac2798adec531ada6a505db5ccb65634cb
vishwajeeta <gs699542@gmail.com>
2024-01-15 13:57:27 +0530

    swapping in java

---
+++ java/swaping.java
@@ -0,0 +1,15 @@
+public class swapping{
+    public static void main(String []args){
+        System.out.println("swapping in java");
+        int a=10;
+        int b=20;
+         System.out.println("Before swapping");
+          System.out.println(a+" "+b);
+        int temp=a;
+        a=b;
+        b=temp;
+         System.out.println("After swapping");
+          System.out.println(a+" "+b);
+
+    }
+}
