bef36853108122b01a81fe5f5599d5b670e99392
vishwajeeta <gs699542@gmail.com>
2025-04-24 21:21:17 +0530

    problem 1

---
+++ Neetcode 150 java/1_dublicate elements/Note.md
@@ -0,0 +1,7 @@
+## other methods:-
+ 1. we can use 2 loop 1 to hold and other to check values against it
+ time complexity is O(n^2)
+ ```java
+ for(int i=0;i<n;i++)
+ for(int j=i+1;j<n;j++)
+```
--- java/_sorting/bubble.java
+++ java/_sorting/bubble.java
@@ -2,7 +2,7 @@ class Main{
     public static void main(String []args){
         int a[]=new int[10];
         int c[]=new int[10];
-        a=[1,9,5,7];
+        a[]=new [1,9,5,7];
         int n=length(a);
         for(int i=0;i<n-2;i++)
         System.out.println(a[0]);
